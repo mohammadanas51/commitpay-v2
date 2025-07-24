63c105d2ed04b994fe1bb2588f3945a5a2f324e9
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-07-24 15:13:17 +0530

    added sub heading

--- src/components/landingPage/header.tsx
+++ src/components/landingPage/header.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Poppins } from "next/font/google";
+
 const poppins = Poppins({
-  weight: ["600"],
+  weight: ["400","600"],
   subsets: ["latin"],
 });
+
 function Header() {
   return (
     <div>
-      <div className="text-center mt-10 text-5xl">
-        <h1 className={poppins.className}>
+      <div className="text-center mt-10">
+        <h1 className={`text-5xl ${poppins.className} font-semibold`}>
           CommitPay - Find your first paid Open Source Issue
         </h1>
-        <p></p>
+        <h2 className={`mt-7 text-2xl ${poppins.className} font-normal`}>
+          First Open Source Contribution? Make It Count (and Make Some Money).
+        </h2>
       </div>
     </div>
   );
