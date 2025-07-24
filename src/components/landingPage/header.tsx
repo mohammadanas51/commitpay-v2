333d5e8fc110fd651a397982d400bcc0882d3dce
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-07-24 12:22:39 +0530

    changed the project structure and created the header component

---
+++ src/components/landingPage/header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Poppins } from "next/font/google";
+const poppins = Poppins({
+  weight: ["600"],
+  subsets: ["latin"],
+});
+function Header() {
+  return (
+    <div>
+      <div className="text-center mt-10 text-5xl">
+        <h1 className={poppins.className}>
+          CommitPay - Find your first paid Open Source Issue
+        </h1>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/landingPage/landingPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './header'
+
+function LandingPage() {
+  return (
+    <Header/>
+  )
+}
+
+export default LandingPage
