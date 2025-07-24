fa1d48e2b9adf1428972c9a86803d957ed6d987d
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-07-24 14:59:39 +0530

    changed the background color

--- src/components/landingPage/header.tsx
+++ src/components/landingPage/header.tsx
@@ -11,7 +11,7 @@ function Header() {
         <h1 className={poppins.className}>
           CommitPay - Find your first paid Open Source Issue
         </h1>
-        
+        <p></p>
       </div>
     </div>
   );
