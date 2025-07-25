4b28548325cf2b11029ab765ee8002d9421ca627
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-07-25 15:45:51 +0530

    added a CTA button

--- src/components/landingPage/landingPage.tsx
+++ src/components/landingPage/landingPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import NavBar from './NavBar'
+import CtaButtons from './CtaButtons';
 
 function LandingPage() {
   return (
     <>
       <NavBar />
       <Header />
+      <CtaButtons/>
     </>
   );
 }
