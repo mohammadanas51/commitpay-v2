1da98eaaa9a010396dc96100b1ff830f8b8aa642
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-07-28 15:44:19 +0530

    join now button linked with the mvp

--- src/components/landingPage/CtaButtons.tsx
+++ src/components/landingPage/CtaButtons.tsx
@@ -12,7 +12,9 @@ function CtaButtons() {
     
     <div className={`flex justify-center items-center mt-8 ${poppins.className} font-semibold text-2xl`} >
       <a
-        href="#cta"
+        href="https://commitpay-mvp.web.app/"
+        target="_blank"
+        rel="noopener noreferrer"
         className="relative inline-block bg-gradient-to-r from-blue-600 to-blue-700 text-white px-10 py-4 rounded-lg hover:from-blue-700 hover:to-blue-800 transition-all duration-300 shadow-lg hover:shadow-blue-500/25 hover:scale-105"
       >
         <span className="relative z-10">Join Now - It's Free</span>
--- src/components/landingPage/Pricing.tsx
+++ src/components/landingPage/Pricing.tsx
@@ -21,15 +21,16 @@ function Pricing() {
       <div className="flex justify-center">
         <div className="flex flex-col rounded-lg bg-slate-800 shadow-sm max-w-96 p-8 my-6 border border-slate-600">
           <div className="pb-8 m-0 mb-8 text-center text-slate-100 border-b border-slate-600">
-            <p className="text-sm uppercase font-semibold text-slate-300">
+            <p className="text-sm uppercase font-semibold text-slate-300 hidden">
               standard
             </p>
             <h1 className="flex justify-center gap-1 mt-4 font-bold text-white text-6xl">
-              <span className="text-3xl">$</span>29
-              <span className="self-end text-3xl">/mo</span>
+              {/* <span className="text-3xl">$</span>29
+              <span className="self-end text-3xl">/mo</span> */}
+              <span className="text-3xl">Coming Soon !</span>
             </h1>
           </div>
-          <div className="p-0">
+          <div className="p-0 hidden">
             <ul className="flex flex-col gap-4">
               <li className="flex items-center gap-4">
                 <span className="p-1 border rounded-full border-slate-500 bg-slate-700">
@@ -133,7 +134,7 @@ function Pricing() {
               className="min-w-32 w-full rounded-md bg-white py-2 px-4 border border-transparent text-center text-sm text-slate-600 transition-all shadow-md hover:shadow-lg focus:bg-white/90 focus:shadow-none active:bg-white/90 hover:bg-white/90 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               type="button"
             >
-              Buy Now
+              Stay Tuned
             </button>
           </div>
         </div>
